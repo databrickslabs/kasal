617439ac9fa8e1c980c0ef018cb9acf9408315c9
nehmetohmedb <nehme.tohme@databricks.com>
2026-02-05 14:37:00 +0100

    refactor(frontend): update common and configuration components

--- src/frontend/src/components/Common/index.ts
+++ src/frontend/src/components/Common/index.ts
@@ -3,4 +3,5 @@ export { GenieSpaceSelector } from './GenieSpaceSelector';
 export { PerplexityConfigSelector } from './PerplexityConfigSelector';
 export { SerperConfigSelector } from './SerperConfigSelector';
 export { MCPServerSelector } from './MCPServerSelector';
-export { default as PaginatedOutput } from './PaginatedOutput'; 
+export { default as PaginatedOutput } from './PaginatedOutput';
+export { default as PlanningOutputFormatter, isPlanningOutput } from './PlanningOutputFormatter'; 
